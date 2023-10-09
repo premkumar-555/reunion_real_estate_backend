198b59b619c3cb66ae3195e9e48db9cbac58ecf1
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-10-09 12:19:01 +0530

    created new mongo cloud database and added connection to db at server using mongoose

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express();
 const cors = require("cors");
 require('dotenv').config();
+const connect = require('./src/DBConfig/connection')
 
 app.use(express.json());
 app.use(cors());
 
 app.listen(3000, async(req, res) => {
     try {
+        await connect()
         console.log("Server Running Successfully!")
     } catch (error) {
         console.log(error.message)
